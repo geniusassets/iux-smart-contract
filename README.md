74cb2d353d905c691407beedd88d9e8f00f2d644
genius-assets-developer <developer@genius-assets.com>
2022-06-27 15:58:23 +0300

    Added tests

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ IUX token has the following capabilities:
 - Name: GeniuX
 - Symbol: IUX
 - Decimals: 18
-- Maximum supply of 1,000,000,000 pre-minted. No more tokens can be issued
-- Ownable: There is a single owner address that can be transferred
-- Burnable: Use the burn or burnFrom function
-- Pausable: The owner can pause the token transfers
-- ForceTransfer: Owner can burn and remint the exact amount of tokens to a different address. Event emitted.
+- Maximum supply of 1,000,000,000 pre-minted. No more tokens can be issued.
+- Ownable: There is a single owner address that can be transferred.
+- Burnable: Use the _burn_ or _burnFrom_ function
+- Pausable: The owner can _pause_ or _resume_ the token transfers
+- ForceTransfer: Owner can _burn_ and _remint_ the exact amount of tokens to a different address. Event emitted _ForceTransfer_
+- The smart-contract is deployed on Polygon network.
+- The token also features _IAntisnipe_ functionality.
 
 Deploy contract:
 `npx hardhat run scripts/deploy-token.js --network matic`
--- test/Token.js
+++ test/Token.js
@@ -8,20 +8,55 @@ describe("Deploy token contract", function () {
   let owner, wallet1, wallet2;
   var exp = ethers.BigNumber.from("10").pow(18);
 
-  beforeEach('Deploy token, vesting, staking', async () => {
+  beforeEach('Deploy token contract', async () => {
     tokenContract = await (await ethers.getContractFactory("IUX")).deploy(ethers.BigNumber.from(1000000).mul(exp));
     await tokenContract.deployed();
-    [owner, wallet1, wallet2] = await ethers.getSigners();
-    const transferToUserWallet = await tokenContract.connect(owner).transfer(wallet1.address, ethers.BigNumber.from(1000).mul(exp));      
-
-    it("Should deploy the token contract", async function () {
-      const totalSupply = await tokenContract.totalSupply();
-      const ownerBalance = await tokenContract.balanceOf(owner.address);    
-      const userBalance = await tokenContract.balanceOf(userWallet1.address);    
-
-      chai.expect(ownerBalance.toString()).to.equal(ethers.BigNumber.from(998000).mul(exp)); 
-      chai.expect(userBalance.toString()).to.equal(ethers.BigNumber.from(1000).mul(exp)); 
-      chai.expect(totalSupply.toString()).to.equal(ethers.BigNumber.from(1000000).mul(exp)); 
-    });  
+    [owner, wallet1, wallet2] = await ethers.getSigners();    
   });
+
+  it("Test the transfer and check balance", async function () { 
+    const transferToUserWallet = await tokenContract.connect(owner).transfer(wallet1.address, ethers.BigNumber.from(1000).mul(exp));  
+    const totalSupply = await tokenContract.totalSupply();
+    const ownerBalance = await tokenContract.balanceOf(owner.address);    
+    const userBalance = await tokenContract.balanceOf(wallet1.address); 
+
+    chai.expect(ownerBalance.toString()).to.equal(ethers.BigNumber.from(999000).mul(exp)); 
+    chai.expect(userBalance.toString()).to.equal(ethers.BigNumber.from(1000).mul(exp)); 
+    chai.expect(totalSupply.toString()).to.equal(ethers.BigNumber.from(1000000).mul(exp)); 
+  });
+
+  it("Test the burn of token", async function () {
+    const transferToUserWallet = await tokenContract.connect(owner).transfer(wallet1.address, ethers.BigNumber.from(1000).mul(exp));  
+    const burnTokens = await tokenContract.connect(wallet1).burn(ethers.BigNumber.from(500).mul(exp));  
+
+    const totalSupply = await tokenContract.totalSupply();
+    const userBalance = await tokenContract.balanceOf(wallet1.address);    
+
+    chai.expect(userBalance.toString()).to.equal(ethers.BigNumber.from(500).mul(exp)); 
+    chai.expect(totalSupply.toString()).to.equal(ethers.BigNumber.from(999500).mul(exp)); 
+  });
+
+  it("Test unauthorized force transfer", async function () {
+    const transferToUserWallet = await tokenContract.connect(owner).transfer(wallet1.address, ethers.BigNumber.from(1000).mul(exp));  
+    await chai.expect
+      (tokenContract
+      .connect(wallet1)
+      .forceTransfer(wallet1.address, wallet2.address, ethers.BigNumber.from(500).mul(exp), ethers.utils.formatBytes32String("0x"))).to.be.revertedWith(
+      "Ownable: caller is not the owner");  
+  });  
+
+  it("Test force transfer from owner", async function () {
+    const transferToUserWallet = await tokenContract.connect(owner).transfer(wallet1.address, ethers.BigNumber.from(1000).mul(exp));  
+    await tokenContract
+      .connect(owner)
+      .forceTransfer(wallet1.address, wallet2.address, ethers.BigNumber.from(500).mul(exp), ethers.utils.formatBytes32String("0x"));
+
+    const totalSupply = await tokenContract.totalSupply();
+    const wallet1Balance = await tokenContract.balanceOf(wallet1.address);    
+    const wallet2Balance = await tokenContract.balanceOf(wallet2.address);    
+
+    chai.expect(wallet1Balance.toString()).to.equal(ethers.BigNumber.from(500).mul(exp)); 
+    chai.expect(wallet2Balance.toString()).to.equal(ethers.BigNumber.from(500).mul(exp));     
+  });    
+
 });
